7aca995a3939f6f975042e3c8d45ed27ce2b5f7e
Devid <devidparmar111@gmail.com>
2026-06-15 18:21:01 +0530

    Add files via upload

---
+++ Program1_HelloWorld.java
@@ -0,0 +1,9 @@
+public class Program1_HelloWorld {
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+        // System.out.println prints text followed by a new line
+        System.out.println("Hello, World!");
+        System.out.print("This will print ");
+        System.out.print("on the same line.");
+    }
+}
